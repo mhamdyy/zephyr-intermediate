ef72122485ced8db21447dc369880c6f09a8f4a5
Mohamed Hamdy <mohamed.hamdy2323@gmail.com>
2026-08-06 20:57:35 +0300

    Task1: Create three preemptive threads at different priorities

--- app/src/main.c
+++ app/src/main.c
@@ -5,27 +5,40 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_A 5
-#define PRIO_B 5
+#define PRIO_LOW  7
+#define PRIO_MED  5
+#define PRIO_HIGH 3
 
-void thread_a_fn(void *p1, void *p2, void *p3)
+void t_low_fn(void *p1, void *p2, void *p3)
 {
-    while (1) {
-        k_msleep(200);
-    }
-}
-
-void thread_b_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
+    while (1)
+    {
+        LOG_INF("T_LOW running tick=%u", k_uptime_get_32());
         k_msleep(300);
     }
 }
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE, thread_a_fn,
-                NULL, NULL, NULL, PRIO_A, 0, 0);
-K_THREAD_DEFINE(thread_b, STACK_SIZE, thread_b_fn,
-                NULL, NULL, NULL, PRIO_B, 0, 0);
+void t_med_fn(void *p1, void *p2, void *p3)
+{
+    while (1)
+    {
+        LOG_INF("T_MED running tick=%u", k_uptime_get_32());
+        k_msleep(200);
+    }
+}
+
+void t_high_fn(void *p1, void *p2, void *p3)
+{
+    while (1)
+    {
+        LOG_INF("T_HIGH running tick=%u", k_uptime_get_32());
+        k_msleep(100);
+    }
+}
+
+K_THREAD_DEFINE(t_low_fn,  STACK_SIZE, t_low_fn,  NULL, NULL, NULL, PRIO_LOW,  0, 0);
+K_THREAD_DEFINE(t_med_fn,  STACK_SIZE, t_med_fn,  NULL, NULL, NULL, PRIO_MED,  0, 0);
+K_THREAD_DEFINE(t_high_fn, STACK_SIZE, t_high_fn, NULL, NULL, NULL, PRIO_HIGH, 0, 0);
 
 int main(void)
 {
