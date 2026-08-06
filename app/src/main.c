9b2c8fa5dd3daf10a6f26e67931a25cbc349c9ae
Mohamed Hamdy <mohamed.hamdy2323@gmail.com>
2026-08-06 23:54:47 +0300

    Add a fourth cooperative thread at priority -1 that does 5 iterations of busy work, then yields

--- app/src/main.c
+++ app/src/main.c
@@ -3,11 +3,12 @@
 
 LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
-#define STACK_SIZE 1024
+#define STACK_SIZE (1024)
 
-#define PRIO_LOW  7
-#define PRIO_MED  5
-#define PRIO_HIGH 3
+#define PRIO_LOW  (7)
+#define PRIO_MED  (5)
+#define PRIO_HIGH (3)
+#define PRIO_COOP (-1)
 
 void t_low_fn(void *p1, void *p2, void *p3)
 {
@@ -36,9 +37,24 @@ void t_high_fn(void *p1, void *p2, void *p3)
     }
 }
 
+void t_coop_fn(void *p1, void *p2, void *p3)
+{
+    LOG_INF("T_COOP Started tick=%u", k_uptime_get_32());
+    for (int i = 0; i < 5; i++)
+    {
+        LOG_INF("T_COOP running step %d/5 tick=%u", i+1, k_uptime_get_32());
+        k_busy_wait(40000);
+    }
+    LOG_INF("T_COOP yield now tick=%u", k_uptime_get_32());
+
+    K_yield();
+    LOG_INF("T_COOP Done tick=%u", k_uptime_get_32());
+}
+
 K_THREAD_DEFINE(thread_low_fn,  STACK_SIZE, t_low_fn,  NULL, NULL, NULL, PRIO_LOW,  0, 0);
 K_THREAD_DEFINE(thread_med_fn,  STACK_SIZE, t_med_fn,  NULL, NULL, NULL, PRIO_MED,  0, 0);
 K_THREAD_DEFINE(thread_high_fn, STACK_SIZE, t_high_fn, NULL, NULL, NULL, PRIO_HIGH, 0, 0);
+K_THREAD_DEFINE(thread_coop_fn, STACK_SIZE, t_coop_fn, NULL, NULL, NULL, PRIO_COOP, 0, 0);
 
 int main(void)
 {
