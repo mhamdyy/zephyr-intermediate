ea35b5c223ba31a0bca0f8a51fd16906f34a5dcb
Mohamed Hamdy <mohamed.hamdy2323@gmail.com>
2026-09-04 23:06:11 +0300

    Code for lecture 4 homework

--- app/src/main.c
+++ app/src/main.c
@@ -5,54 +5,99 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE (1024)
 #define PRIO (7)
-#define INCREMENTS (1000000)
 
-static struct k_sem sem_finished;
-static volatile int counter = 0;
+typedef struct _sensor_data {
+    int32_t temperature_mc;
+    uint32_t timestamp_ms;
+    uint8_t seq;
+}sensor_data;
 
-static K_MUTEX_DEFINE(mutex_for_shared_data);
+void display_listener_cb(const struct zbus_channel *chan);
 
-void t_fn(void *p1, void *p2, void *p3)
+ZBUS_LISTENER_DEFINE(fast_display_listner, display_listener_cb);
+
+ZBUS_MSG_SUBSCRIBER_DEFINE(slow_logger_subscriber);
+
+ZBUS_CHAN_DEFINE(sensor_channel, sensor_data, NULL, NULL,
+                 ZBUS_OBSERVERS(fast_display_listner, slow_logger_subscriber),
+                 ZBUS_MSG_INIT(.temperature_mc = 0,
+                               .timestamp_ms = 0,
+                               .seq = 0));
+
+void display_listener_cb(const struct zbus_channel *chan)
 {
-    const char *name = k_thread_name_get(k_current_get());
-    LOG_INF("%s Started", name);
+    const sensor_data *msg = (const sensor_data *)zbus_chan_const_msg(chan);
 
-    for (int i = 0; i < INCREMENTS; i++)
-    {
-        k_mutex_lock(&mutex_for_shared_data, K_FOREVER);
-        counter ++;
-        k_mutex_unlock(&mutex_for_shared_data);
-    }
-
-    LOG_INF("%s Finished", name);
-    k_sem_give(&sem_finished);
+    LOG_INF("[DISPLAY-LISTNER] thread=%s seq=%u temp=%d mC",
+            k_thread_name_get(k_current_get()),
+            msg->seq,
+            msg->temperature_mc);
 }
 
+void sensor_thread_fn(void *p1, void *p2, void *p3)
+{
+    ARG_UNUSED(p1); ARG_UNUSED(p2); ARG_UNUSED(p3);
 
-K_THREAD_DEFINE(thread_a,  STACK_SIZE, t_fn,  NULL, NULL, NULL, PRIO,  0, 0);
-K_THREAD_DEFINE(thread_b,  STACK_SIZE, t_fn,  NULL, NULL, NULL, PRIO,  0, 0);
+    k_thread_name_set(k_current_get(), "sensor");
+
+    ensor_data data;
+
+    while(1)
+    {
+        data.temperature_mc = 24000 + (i * 350);
+        data.timestamp_ms = k_uptime_get_32();
+        data.seq = (uint8_t)i;
+
+        LOG_INF("[SENSOR] publish seq=%u temp=%d mC",
+                data.seq,
+                data.temperature_mc);
+
+        int ret = zbus_chan_pub(&sensor_chan, &data, K_MSEC(100));
+
+        if (ret != 0)
+        {
+            LOG_WRN("[SENSOR] publish failed ret=%d", ret);
+        }
+
+        k_msleep(100);
+    }
+}
+
+void logger_thread_fn(void *p1, void *p2, void *p3)
+{
+    ARG_UNUSED(p1); ARG_UNUSED(p2); ARG_UNUSED(p3);
+
+    k_thread_name_set(k_current_get(), "logger");
+
+    const struct zbus_channel *chan;
+
+    while (1)
+    {
+        sensor_data msg;
+
+        int ret = zbus_sub_wait_msg(&logger_sub, &chan, &msg, K_MSEC(1500));
+
+        if (ret != 0)
+        {
+            LOG_WRN("[LOGGER-MSG] timeout ret=%d", ret);
+            break;
+        }
+
+        LOG_INF("[LOGGER-MSG] thread=%s seq=%u temp=%d latency=%ums",
+                k_thread_name_get(k_current_get()),
+                msg.seq,
+                msg.temperature_mc,
+                k_uptime_get_32() - msg.timestamp_ms);
+
+        k_msleep(350);
+    }
+}
+
+K_THREAD_DEFINE(sensor_thread,  STACK_SIZE, sensor_thread_fn,  NULL, NULL, NULL, PRIO,  0, 0);
+K_THREAD_DEFINE(logger_thread,  STACK_SIZE, logger_thread_fn,  NULL, NULL, NULL, PRIO,  0, 0);
 
 
 int main(void)
 {
-    LOG_INF("Main Function Started");
-    k_sem_init(&sem_finished, 0, 1);
-
-    LOG_INF("Expected final value: %d", INCREMENTS * 2);
-
-    k_sem_take(&sem_finished, K_FOREVER);
-    k_sem_take(&sem_finished, K_FOREVER);
-
-    LOG_INF("Actual final value: %u", counter);
-
-    if (counter == INCREMENTS * 2)
-    {
-        LOG_WRN("No race this run");
-    }
-    else
-    {
-        LOG_ERR("Race condition confirmed: lost %d updates", (INCREMENTS * 2) - counter);
-    }
     return 0;
 }
-
