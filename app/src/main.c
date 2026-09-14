86908375b0c4c12901f8ed67ba2301dff47db353
mhamdyy <mohamed.hamdy2323@gmail.com>
2026-09-14 20:25:02 +0300

    initial implementation for task watchdog

--- app/src/main.c
+++ app/src/main.c
@@ -1,6 +1,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/zbus/zbus.h>
+#include <zephyr/task_wdt/task_wdt.h>
 
 LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
@@ -35,15 +36,27 @@ void display_listener_cb(const struct zbus_channel *chan)
             msg->temperature_mc);
 }
 
+void sensor_wgt_miss()
+{
+    LOG_ERR("Sensor thread watchdog expired");
+}
+
+void logger_wgt_miss()
+{
+    LOG_ERR("Logger thread watchdog expired");
+}
+
 void sensor_thread_fn(void *p1, void *p2, void *p3)
 {
     ARG_UNUSED(p1); ARG_UNUSED(p2); ARG_UNUSED(p3);
 
+    int wdt_sensor = task_wdt_add(2000, sensor_wgt_miss, (void*) k_current_get());
+
     k_thread_name_set(k_current_get(), "sensor");
 
     sensor_data data;
-    int i =0;
-    while(1)
+
+    for (int i =0; i< 10;i++)
     {
         data.temperature_mc = 24000 + (i * 350);
         data.timestamp_ms = k_uptime_get_32();
@@ -59,9 +72,10 @@ void sensor_thread_fn(void *p1, void *p2, void *p3)
         {
             LOG_WRN("[SENSOR] publish failed ret=%d", ret);
         }
-        i++;
         k_msleep(100);
+        task_wdt_feed(wdt_sensor);
     }
+    task_wdt_delete(wdt_sensor);
 }
 
 void logger_thread_fn(void *p1, void *p2, void *p3)
@@ -69,6 +83,8 @@ void logger_thread_fn(void *p1, void *p2, void *p3)
     ARG_UNUSED(p1); ARG_UNUSED(p2); ARG_UNUSED(p3);
 
     k_thread_name_set(k_current_get(), "logger");
+
+    int wdt_logger = task_wdt_add(2000, logger_wgt_miss, (void*) k_current_get());
 
     const struct zbus_channel *chan;
 
@@ -90,15 +106,19 @@ void logger_thread_fn(void *p1, void *p2, void *p3)
                 msg.temperature_mc,
                 k_uptime_get_32() - msg.timestamp_ms);
 
-        k_msleep(350);
+        k_msleep(3500);
+        task_wdt_feed(wdt_logger);
     }
+    task_wdt_delete(wdt_logger);
+
 }
 
-K_THREAD_DEFINE(sensor_thread,  STACK_SIZE, sensor_thread_fn,  NULL, NULL, NULL, PRIO,  0, 0);
-K_THREAD_DEFINE(logger_thread,  STACK_SIZE, logger_thread_fn,  NULL, NULL, NULL, PRIO,  0, 0);
-
+K_THREAD_DEFINE(sensor_thread, STACK_SIZE, sensor_thread_fn, NULL, NULL, NULL, PRIO, 0, 0);
+K_THREAD_DEFINE(logger_thread, STACK_SIZE, logger_thread_fn, NULL, NULL, NULL, PRIO, 0, 0);
+K_THREAD_DEFINE(health_thread, STACK_SIZE, health_thread_fn, NULL, NULL, NULL, PRIO, 0, 0);
 
 int main(void)
 {
+    task_wdt_init(NULL);
     return 0;
 }
