9c38242fdf8545b8b158890fec426e7797295918
Mohamed Hamdy <mohamed.hamdy2323@gmail.com>
2026-09-23 23:52:20 +0300

    change the zbus to MSG Queue pipeline

--- app/src/main.c
+++ app/src/main.c
@@ -5,8 +5,10 @@
 
 LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
-#define STACK_SIZE (2048)
-#define PRIO (7)
+#define STACK_SIZE              (2048)
+#define PRIO                    (7)
+#define PIPELINE_QUEUE_DEPTH    (6)
+#define SENSOR_DATA_COUNT       (20)
 
 typedef struct _sensor_data {
     int32_t temperature_mc;
@@ -14,27 +16,7 @@ typedef struct _sensor_data {
     uint8_t seq;
 }sensor_data;
 
-void display_listener_cb(const struct zbus_channel *chan);
-
-ZBUS_LISTENER_DEFINE(fast_display_listner, display_listener_cb);
-
-ZBUS_MSG_SUBSCRIBER_DEFINE(logger_sub);
-
-ZBUS_CHAN_DEFINE(sensor_channel, struct _sensor_data, NULL, NULL,
-                 ZBUS_OBSERVERS(fast_display_listner, logger_sub),
-                 ZBUS_MSG_INIT(.temperature_mc = 0,
-                               .timestamp_ms = 0,
-                               .seq = 0));
-
-void display_listener_cb(const struct zbus_channel *chan)
-{
-    const sensor_data *msg = (const sensor_data *)zbus_chan_const_msg(chan);
-
-    LOG_INF("[DISPLAY-LISTNER] thread=%s seq=%u temp=%d mC",
-            k_thread_name_get(k_current_get()),
-            msg->seq,
-            msg->temperature_mc);
-}
+K_MSGQ_DEFINE(pipeline_queue, sizeof(sensor_data), PIPELINE_QUEUE_DEPTH, 4);
 
 void sensor_wgt_miss()
 {
@@ -56,25 +38,24 @@ void sensor_thread_fn(void *p1, void *p2, void *p3)
 
     sensor_data data;
 
-    for (int i =0; i< 10;i++)
+    for (int i =0; i< SENSOR_DATA_COUNT; i++)
     {
         data.temperature_mc = 24000 + (i * 350);
         data.timestamp_ms = k_uptime_get_32();
         data.seq = (uint8_t)i;
 
-        LOG_INF("[SENSOR] publish seq=%u temp=%d mC",
-                data.seq,
-                data.temperature_mc);
+        LOG_INF("[SENSOR] puts seq=%u temp=%d mC", data.seq, data.temperature_mc);
 
-        int ret = zbus_chan_pub(&sensor_channel, &data, K_MSEC(100));
+        int ret = k_msgq_put(&pipeline_queue, &data, K_MSEC(200));
 
         if (ret != 0)
         {
-            LOG_WRN("[SENSOR] publish failed ret=%d", ret);
+            LOG_WRN("[SENSOR] put failed ret=%d", ret);
         }
-        k_msleep(100);
+        k_msleep(50);
         task_wdt_feed(wdt_sensor);
     }
+
     task_wdt_delete(wdt_sensor);
 }
 
@@ -86,13 +67,11 @@ void logger_thread_fn(void *p1, void *p2, void *p3)
 
     int wdt_logger = task_wdt_add(2000, logger_wgt_miss, (void*) k_current_get());
 
-    const struct zbus_channel *chan;
-
-    while (1)
+    for (int i = 0; i < SENSOR_DATA_COUNT; i++)
     {
         sensor_data msg;
 
-        int ret = zbus_sub_wait_msg(&logger_sub, &chan, &msg, K_MSEC(1500));
+        int ret = k_msgq_get(&pipeline_queue, &msg, K_MSEC(300));
 
         if (ret != 0)
         {
@@ -106,11 +85,10 @@ void logger_thread_fn(void *p1, void *p2, void *p3)
                 msg.temperature_mc,
                 k_uptime_get_32() - msg.timestamp_ms);
 
-        k_msleep(3500);
+        k_msleep(350);
         task_wdt_feed(wdt_logger);
     }
     task_wdt_delete(wdt_logger);
-
 }
 
 K_THREAD_DEFINE(sensor_thread, STACK_SIZE, sensor_thread_fn, NULL, NULL, NULL, PRIO, 0, 0);
